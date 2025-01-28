254be2ce03fb21bdbaeea8a2bae9e7d5a11c3266
Fat7allah <yassine.fat7allah@gmail.com>
2025-01-28 23:47:10 +0100

    Create hooks.py

---
+++ mgen_management/mgen_management/hooks.py
@@ -0,0 +1,15 @@
+app_name = "mgen_management"
+app_title = "Mgen Management"
+app_publisher = "Fat7allah"
+app_description = "Mgen Management"
+app_email = "fat7allah.habbani@gmail.com"
+app_license = "MIT"
+
+fixtures = [
+    {
+        "doctype": "Profession",
+        "filters": [
+            ["name", "like", "%"]
+        ]
+    }
+]
