cdf342a00af7d834f969e27c16b0c66ed7e2fa28
Fat7allah <yassine.fat7allah@gmail.com>
2025-01-30 00:39:16 +0100

    add income doctype

---
+++ mgen_management/mgen_management/doctype/income/income.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2025, Fat7allah and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+class Income(Document):
+    def validate(self):
+        # Validate that province belongs to selected region
+        if self.region and self.province:
+            province_doc = frappe.get_doc("Province", self.province)
+            if province_doc.region != self.region:
+                frappe.throw(f"الإقليم {self.province} لا ينتمي إلى الجهة {self.region}")
