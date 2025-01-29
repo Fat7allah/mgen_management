d4c27a43315fcf9414fc82a0d4d92fd04f5bfebf
Fat7allah <yassine.fat7allah@gmail.com>
2025-01-30 00:29:28 +0100

    add expense doctype

---
+++ mgen_management/mgen_management/doctype/expense/expense.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2025, Fat7allah and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Expense', {
+	refresh: function(frm) {
+
+	},
+	
+	region: function(frm) {
+		// Clear province when region changes
+		frm.set_value('province', '');
+		
+		// Update province filter based on selected region
+		frm.set_query('province', function() {
+			return {
+				filters: {
+					'region': frm.doc.region
+				}
+			};
+		});
+	}
+});
