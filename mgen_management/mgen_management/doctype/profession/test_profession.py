aafaf50cdecfe68dc4cd9e6bb4ccf791c8720c6a
Fat7allah <yassine.fat7allah@gmail.com>
2025-01-30 00:06:34 +0100

    update profession doctype

---
+++ mgen_management/mgen_management/doctype/profession/test_profession.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Fat7allah and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestProfession(FrappeTestCase):
+	pass
