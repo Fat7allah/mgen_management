28bca6c22665c2bfe7050c667c1cbc103bb2402f
Fat7allah <yassine.fat7allah@gmail.com>
2025-02-01 16:37:32 +0100

    Update income.js

--- mgen_management/mgen_management/doctype/income/income.js
+++ mgen_management/mgen_management/doctype/income/income.js
@@ -3,7 +3,11 @@
 
 frappe.ui.form.on('Income', {
 	refresh: function(frm) {
-
+		// Initial setup of the field based on current value
+		if (frm.doc.type === 'بطائق') {
+			frm.set_df_property('amount', 'read_only', 1);
+			calculate_amount(frm);
+		}
 	},
 	
 	region: function(frm) {
@@ -18,5 +22,28 @@ frappe.ui.form.on('Income', {
 				}
 			};
 		});
+	},
+
+	type: function(frm) {
+		// When type changes
+		if (frm.doc.type === 'بطائق') {
+			frm.set_df_property('amount', 'read_only', 1);
+			calculate_amount(frm);
+		} else {
+			frm.set_df_property('amount', 'read_only', 0);
+		}
+	},
+	
+	quantity: function(frm) {
+		// When quantity changes and type is بطائق
+		if (frm.doc.type === 'بطائق') {
+			calculate_amount(frm);
+		}
 	}
 });
+
+function calculate_amount(frm) {
+	// Calculate amount as quantity × 100
+	let amount = (frm.doc.quantity || 0) * 100;
+	frm.set_value('amount', amount);
+}
